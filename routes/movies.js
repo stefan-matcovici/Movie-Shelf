9ed4d4b66bf3e2945acd3eb7d829e9143e96f673
stefan-matcovici <stefan.matcovici@yahoo.com>
2018-04-01 20:15:41 +0300

    Implemented uploading images to google cloud bucket

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let router = express.Router();
+const images = require('../lib/images');
 
 function getMovieModel() {
     return require(`../models/movie-model-${require('../config').get('DATA_BACKEND')}.js`);
 }
 
-/* GET home page. */
 router.get('/list', function(req, res, next) {
     getMovieModel().list(10, req.query.pageToken, (err, entities, cursor) => {
         if (err) {
@@ -24,17 +24,30 @@ router.get('/add', function(req, res, next) {
     res.render('movies/add');
 });
 
-router.post('/add', (req, res, next) => {
-    const data = req.body;
 
-    getMovieModel().create(data, (err, savedData) => {
-        if (err) {
-            next(err);
-            return;
+router.post(
+    '/add',
+    images.multer.single('image'),
+    images.sendUploadToGCS,
+    (req, res, next) => {
+        let data = req.body;
+
+        // Was an image uploaded? If so, we'll use its public URL
+        // in cloud storage.
+        if (req.file && req.file.cloudStoragePublicUrl) {
+            data.imageUrl = req.file.cloudStoragePublicUrl;
         }
-        res.redirect(`${req.baseUrl}/${savedData.id}`);
-    });
-});
+
+        // Save the data to the database.
+        getMovieModel().create(data, (err, savedData) => {
+            if (err) {
+                next(err);
+                return;
+            }
+            res.redirect(`${req.baseUrl}/${savedData.id}`);
+        });
+    }
+);
 
 router.get('/:book', (req, res, next) => {
     getMovieModel().read(req.params.book, (err, entity) => {

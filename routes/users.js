459d526cccf48b486697b0dbf758ae59ac37f1b9
stefan-matcovici <stefan.matcovici@yahoo.com>
2018-04-01 12:23:16 +0300

    Added afterlogin processing and testing session

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,33 @@ let passport = require('passport');
 router.post('/login',
     passport.authenticate('local', {
         successRedirect: '/loginSuccess',
-        failureRedirect: '/loginFailure'
+        failureRedirect: '/login',
+        failureFlash : true
     })
 );
 
 router.get('/login', (req, res) => {
-    res.render('login', {
-        user: {},
-        action: 'login'
-    });
+    res.render('login', { message: req.flash('loginMessage') });
 });
+
+router.get('/loginFailure', function(req, res, next) {
+    res.send('Failed to authenticate');
+});
+
+router.get('/loginSuccess', function(req, res, next) {
+    res.send('Successfully authenticated');
+});
+
+router.get('/protectedResource', isLoggedIn, function(req, res) {
+    res.send("You are cool");
+});
+
+function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/login');
+}
 
 module.exports = router;
